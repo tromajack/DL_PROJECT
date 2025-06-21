27c4d05cb7d7685499b8decb1fa83b4f0199f1fa
tromajack <prajwaln070@gmail.com>
2025-06-21 15:12:48 +0530

    Data ingestion completed

---
+++ main.py
@@ -0,0 +1,15 @@
+from CNNCLASSIFIER.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from CNNCLASSIFIER import logger
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/CNNCLASSIFIER/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,31 @@
+from CNNCLASSIFIER.config.configuration import ConfigurationManager
+from CNNCLASSIFIER.components.data_ingestion import DataIngestion
+from CNNCLASSIFIER import logger
+
+
+STAGE_NAME= "DATA INGESTION STAGE"
+
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
