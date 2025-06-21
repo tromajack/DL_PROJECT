78c008d3017d41aaa525cd333caaaeb44e68c302
tromajack <prajwaln070@gmail.com>
2025-06-21 17:49:32 +0530

    Training completed

--- src/CNNCLASSIFIER/config/configuration.py
+++ src/CNNCLASSIFIER/config/configuration.py
@@ -2,8 +2,8 @@ import os
 from CNNCLASSIFIER.constants import *
 from CNNCLASSIFIER.utils.common import read_yaml, create_directories
 from CNNCLASSIFIER.entity.config_entity import (DataIngestionConfig,
-                                                PrepareBaseModelConfig)
-                                                # TrainingConfig,
+                                                PrepareBaseModelConfig,
+                                                TrainingConfig)
                                                 # EvaluationConfig)
 
 
@@ -55,27 +55,27 @@ class ConfigurationManager:
     
 
 
-    # def get_training_config(self) -> TrainingConfig:
-    #     training = self.config.training
-    #     prepare_base_model = self.config.prepare_base_model
-    #     params = self.params
-    #     training_data = os.path.join(self.config.data_ingestion.unzip_dir, "Chest-CT-Scan-data")
-    #     create_directories([
-    #         Path(training.root_dir)
-    #     ])
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "Chest-CT-Scan-data")
+        create_directories([
+            Path(training.root_dir)
+        ])
 
-    #     training_config = TrainingConfig(
-    #         root_dir=Path(training.root_dir),
-    #         trained_model_path=Path(training.trained_model_path),
-    #         updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
-    #         training_data=Path(training_data),
-    #         params_epochs=params.EPOCHS,
-    #         params_batch_size=params.BATCH_SIZE,
-    #         params_is_augmentation=params.AUGMENTATION,
-    #         params_image_size=params.IMAGE_SIZE
-    #     )
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
 
-    #     return training_config
+        return training_config
     
 
 
